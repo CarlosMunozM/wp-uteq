178cd1bc45dd018ef6f8aaa0b964ac62271829b1
Carlos Muñoz <carlos-luis-m.m@hotmail.com>
2026-08-17 17:02:58 -0500

    Cambio de vista de bola de empleo

--- components/body/BodyBolsaEmpleos.jsx
+++ components/body/BodyBolsaEmpleos.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useMemo } from "react";
 import axios from 'axios';
-import { WS_LIST_JOBS_BUSN, IMG_JOBS_BS_FOLDER } from 'config';
+import { WS_LIST_JOBS_BUSN, IMG_JOBS_BS_FOLDER, WS_LIST_SIX_NEWS_OF_UNIVERSITY, NEWS_UNIV_IMGS_FOLDER } from 'config';
 import ReactPaginate from 'react-paginate';
+import { v4 as uuidv4 } from 'uuid';
 
 
 export { BodyBolsaEmpleos };
@@ -37,6 +38,112 @@ async function make_request_ws(path_url) {
     return (listTemp);
 }
 
+function changeFormatMonth(mes, language) {
+    switch (mes) {
+        case "1":
+        case "01":
+            return (language === "es" ? "Enero" : (language === "en" ? "January" : "Janeiro"));
+        case "2":
+        case "02":
+            return (language === "es" ? "Febrero" : (language === "en" ? "February" : "Fevereiro"));
+        case "3":
+        case "03":
+            return (language === "es" ? "Marzo" : (language === "en" ? "March" : "Março"));
+        case "4":
+        case "04":
+            return (language === "es" ? "Abril" : (language === "en" ? "April" : "Abril"));
+        case "5":
+        case "05":
+            return (language === "es" ? "Mayo" : (language === "en" ? "May" : "Maio"));
+        case "6":
+        case "06":
+            return (language === "es" ? "Junio" : (language === "en" ? "June" : "Junho"));
+        case "7":
+        case "07":
+            return (language === "es" ? "Julio" : (language === "en" ? "July" : "Julho"));
+        case "8":
+        case "08":
+            return (language === "en" ? "August" : "Agosto");
+        case "9":
+        case "09":
+            return (language === "es" ? "Septiembre" : (language === "en" ? "September" : "Setembro"));
+        case "10":
+            return (language === "es" ? "Octubre" : (language === "en" ? "October" : "Outubro"));
+        case "11":
+            return (language === "es" ? "Noviembre" : (language === "en" ? "November" : "Novembro"));
+        case "12":
+            return (language === "es" ? "Diciembre" : (language === "en" ? "December" : "Dezembro"));
+    }
+}
+
+function ItemNews(props) {
+    return (<>
+        <div className="col-md-6">
+            <a href={`/${props.language}/comunicacion/noticia/${props.url.trim()}`} target="_blank" aria-label="link noticia indv" data-toggle="tooltip" data-placement="bottom" className="link-news-dep" title={props.titular.trim()}>
+                <div className="p-3 pnl-other-news">
+                    <div className="row">
+                        <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                            <img alt="" className="img-portrait-news" src={`${NEWS_UNIV_IMGS_FOLDER}${props.urlportada.trim()}`} />
+                        </div>
+                        <div className="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                            <div className="text-news-other">
+                                {props.titular.trim()}
+                            </div>
+                            <div className="date-news">
+                                <span className="badge sticker-tipo-dept" style={{ backgroundColor: "#025a27", color: "#FFF" }}>{props.departamento.trim() !== 'Universidad' ? props.departamento.trim() : 'Institucional'}</span>&nbsp;&nbsp;
+                                <i className="fa fa-history"></i>&nbsp;&nbsp;{`${props.fecha.substr(8, 2)} de ${changeFormatMonth(props.fecha.substr(5, 2), props.language)} del ${props.fecha.substr(0, 4)}`}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </>)
+}
+
+const listItemsNewsPanel = (dataItems, language) => {
+    return (
+        dataItems?.map((item) => {
+            // Extrae el primer departamento o categoría si existen
+            const primerDept = item.listaDepartamentos?.[0];
+            const primeraCat = item.listaCategoriasNotc?.[0];
+
+            const colordep = primerDept?.dpColor?.trim()
+                || primeraCat?.gtColorIdentf?.trim()
+                || "#025a27";
+
+            let deptName = '';
+
+            if (primerDept) {
+                deptName = language === "es"
+                    ? (primerDept.dpNombre?.trim() !== 'Universidad' ? primerDept.dpNombre?.trim() : 'Institucional')
+                    : (language === "en"
+                        ? (primerDept.dpNombreEn?.trim() !== 'University' ? primerDept.dpNombreEn?.trim() : 'Institutional')
+                        : (primerDept.dpNombrePt?.trim() !== 'Universidade' ? primerDept.dpNombrePt?.trim() : 'Institucional'));
+            } else if (primeraCat) {
+                deptName = language === "es"
+                    ? primeraCat.gtTitular?.trim()
+                    : (language === "en" ? primeraCat.gtTitularEn?.trim() : primeraCat.gtTitularPt?.trim());
+            } else {
+                deptName = language === "en" ? 'Institutional' : 'Institucional';
+            }
+
+            return (
+                <ItemNews
+                    key={uuidv4()}
+                    url={item.ntUrlNoticia}
+                    titular={language === "es" ? item.ntTitular : (language === "en" ? item.ntTitularEn : item.ntTitularPt)}
+                    urlportada={item.ntUrlPortada}
+                    colordep={colordep}
+                    departamento={deptName}
+                    fecha={item.ntFecha}
+                    language={language}
+                />
+            );
+        })
+    );
+};
+
 function BodyBolsaEmpleos(data) {
     const itemsPerPage = 4;
     const [dataempls, setDataempls] = useState([]);
@@ -45,6 +152,8 @@ function BodyBolsaEmpleos(data) {
     const [endOffset, setEndOffset] = useState(0);
     const [itemOffset, setItemOffset] = useState(0);
     const [estdListEmplys, setEstdListEmplys] = useState(false);
+
+    const [dataSixNews, setDataSixNews] = useState([]);
 
     useEffect(() => {
         (async () => {
@@ -56,6 +165,14 @@ function BodyBolsaEmpleos(data) {
             setEstdListEmplys(result.data.length > 0);
         })();
     }, [itemOffset, itemsPerPage, endOffset]);
+
+    useEffect(() => {
+        (async () => {
+            const result = await make_request_ws(`${WS_LIST_SIX_NEWS_OF_UNIVERSITY}0`);
+            setDataSixNews((result.data !== null && result.data !== "") ? result.data : []);
+        })();
+    }, []);
+
 
     function EmploymentsPanelsForPage({ currentItems }) {
         return (
@@ -82,11 +199,30 @@ function BodyBolsaEmpleos(data) {
             <h2 className="title-cont-page text-center">{data.language === "es" ? data.data8.pwNombre.trim() : (data.language === "en" ? data.data8.pwNombreEn.trim() : data.data8.pwNombrePt.trim())}</h2>
             <div className="col-md-12 mt-2">
                 {
+                    /*
                     estdListEmplys ? (<div className="row"><EmploymentsPanelsForPage currentItems={currentItems} /></div>) : (<div className="row g-0"><div className="ratio ratio-21x9">
                         <img src={`/assets/img/${data.language === "es" ? "bolsa-de-empleo-banner-uteq-es.jpg" : (data.language === "en" ? "bolsa-de-empleo-banner-uteq-en.jpg" : "bolsa-de-empleo-banner-uteq-pt.jpg")}`} className="d-block w-100"
                             alt={data.language === "es" ? "Ofertas de empleo" : (data.language === "en" ? "Job vacancies" : "Ofertas de emprego")} />
                     </div></div>)
+                    */
+                    (<div className="row g-0">
+                        <div className="ratio ratio-21x9">
+                            <a
+                                href="https://sga.uteq.edu.ec/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="d-block w-100 h-100"
+                            >
+                                <img
+                                    src={`/assets/img/${data.language === "es" ? "bolsa-de-empleo-sga-banner-uteq-es.jpg" : (data.language === "en" ? "bolsa-de-empleo-sga-banner-uteq-en.jpg" : "bolsa-de-empleo-sga-banner-uteq-pt.jpg")}`}
+                                    className="d-block w-100 h-100 object-fit-cover"
+                                    alt={data.language === "es" ? "Ofertas de empleo" : (data.language === "en" ? "Job vacancies" : "Ofertas de emprego")}
+                                />
+                            </a>
+                        </div>
+                    </div>)
                 }
+                {/*
                 <div className="row mt-3">
                     <div className="col-md-12 d-flex justify-content-center">
                         {
@@ -114,6 +250,18 @@ function BodyBolsaEmpleos(data) {
                         }
                     </div>
                 </div>
+                */}
+            </div>
+
+            <div className="row g-0 mt-3">
+                {
+                    dataSixNews.length > 0 && (<>
+                        <h2 className="title-cont-page text-center mt-2">{data.language === "es" ? "Noticias" : (data.language === "en" ? "News" : "Notícias")}</h2>
+                        <div className="row g-1">
+                            {listItemsNewsPanel(dataSixNews, data.language)}
+                        </div>
+                    </>)
+                }
             </div>
         </div>
     </>);
